c758018522f53621b260827dcd5889e55b87e266
FinlayWSCW <159732365+Watsonfi1@users.noreply.github.com>
2024-05-06 10:40:23 +1200

    Version 2

--- main2.py
+++ main2.py
@@ -1,3 +1,5 @@
+#setting values
+score = 0
 #getting persons name
 print("Hello Random Person")
 name = input("what is your name? ")
@@ -8,7 +10,25 @@ answer1 = input("What country has veritcal red, white and blue stripes on its fl
 #check the answer
 if("france" in answer1.lower()):
     print("You answered correctly!")
+    score = score + 1
+else:
+    print("You are incorrect, the answer was France.")
+#other questions
+answer2 = input("What country has the only flag that isn't square? ? ")
+#check the answer
+if("nepal" in answer2.lower()):
+    print("You answered correctly!")
+    score = score + 1
+else:
+    print("You are incorrect, the answer was Nepal.")
+answer3 = input("What is the most common colour on flags? ")
+#check the answer
+if("red" in answer3.lower()):
+    print("You answered correctly!")
+    score = score + 1
 else:
     print("You are incorrect, the answer was France.")
 #finish the quiz
+print("You got", score,"out of 3 correct on the flag quiz")
 print("Thanks for playing the Flag Quiz")
+
