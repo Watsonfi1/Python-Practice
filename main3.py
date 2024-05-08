b2652c0a1e2d2d0e5da455376afea2e42e48f4a3
FinlayWSCW <159732365+Watsonfi1@users.noreply.github.com>
2024-05-09 10:29:35 +1200

    Version 3

--- main3.py
+++ main3.py
@@ -1,5 +1,10 @@
 #setting values
 score = 0
+a2 = "red"
+b2 = "blue"
+c2 = "white"
+list1 = [a2, "a"]
+list2 = [b2, "b", c2, "c"]
 #getting persons name
 print("Hello Random Person")
 name = input("what is your name? ")
@@ -23,14 +28,21 @@ elif(answer2 == ""):
     print("That is not a answer to the question. The answer was Nepal")
 else:
     print("You are incorrect, the answer was Nepal.")
-answer3 = input("What is the most common colour on flags? ")
+answer3 = input("What is the most common colour on flags?\nA. {}\nB. {}\nC. {}\n".format(a2, b2, c2))
 #check the answer
-if("red" in answer3.lower()):
+if(answer3.lower() in list1):
     print("You answered correctly!")
     score += 1
+elif(answer3.lower() in list2):
+    print("You answered incorrectly. The answer was red")
 else:
-    print("You are incorrect, the answer was Red.")
+    print("Your input is not in the question")
 #finish the quiz
-print("You got", score,"out of 3 correct on the flag quiz")
+if(score == 3):
+    print("Congrats {} you got {} out of 3 correct on the flag quiz".format(name, score))
+elif(score == 2):
+    print("Nice try {} you got {} out of 3 correct on the flag quiz".format(name, score))
+else:
+    print("Nice try {} you got {} out of 3 correct on the flag quiz".format(name, score))
 print("Thanks for playing the Flag Quiz")
 
