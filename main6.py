a42061795fb3ddcb7e49b30db239a69396f6504a
FinlayWSCW <159732365+Watsonfi1@users.noreply.github.com>
2024-05-23 10:26:22 +1200

    version 6

--- main6.py
+++ main6.py
@@ -11,17 +11,25 @@ def password():
         if password == 3874:
             return True
     return False
+
 def intro():
+    global name
     name = input("What is your name? ")
     print("Welcome {} to the quiz".format(name))
+
 def questions(question,correctanswer):
     global livesvar
-    answer = input(question)
-    if answer == correctanswer:
-        print("Correct answer")
-    else: 
-        print("Incorrect answer")
-        print("You have {} lives left.".format(livesvar))
+    global score
+    if livesvar > 0:
+        answer = input(question)
+        if answer.lower() == correctanswer:
+            print("Correct answer")
+            score += 1
+        else: 
+            print("Incorrect answer")
+            livesvar -= 1
+            print("You have {} lives left.".format(livesvar))
+
 def lives():
     global livesvar
     NUMBERS = [1,2,3,4]
@@ -29,18 +37,43 @@ def lives():
         livesvar = input("How many lives would you like? 4 max ")
         try:
             livesvar = int(livesvar)
-            if NUMBERS in livesvar:
+            if livesvar in NUMBERS:
                 return
         except:
             print("Thats not a valid amount of lives")
 
+def playagain():
+    global play
+    global score
+    global name
+    print("You got {} out of 3 {} well done!!!".format(score,name))
+    while True:
+        playagain3 = input("Would you like to play again? ")
+        if playagain3.lower() == "yes":
+            play = True
+            return
+        elif playagain3.lower() == "no":
+            print("Thank you for playing the Flag Quiz")
+            play = False
+            return 
+        else:
+            print("Please answer with Yes or No")
+#-------------Values-----------
+QUESTIONS = ["What country has veritcal red, white and blue stripes on its flag? ",
+                "What country has the only flag that isn't rectangular? ",
+                    "What is the most common colour on flags?"]
+play = True
 
 
 
-
-#Main Code
+#-----------Main Code---------
 passwordstate = password()
 if passwordstate == True:
     intro()
-    
-    questions()
+    while play == True:
+        score = 0
+        lives()
+        questions(QUESTIONS[0],"france")
+        questions(QUESTIONS[1],"nepal")
+        questions(QUESTIONS[2],"red")
+        playagain()
